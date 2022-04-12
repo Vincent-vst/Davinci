6af8b3e9072c7a952bc5bf4c1733aabc0300b400
Vincent-vst <vincent.descatoire@gmail.com>
2022-04-12 09:34:47 +0000

    heroku procfile

--- src/client.py
+++
@@ -1,9 +0,0 @@
-from urllib import response
-import requests 
-
-url = "http://127.0.0.1:5000/transcript/" 
-
-# response = requests.get(url)
-response = requests.post(url + "avion.mp3")
-
-print(response.json())
