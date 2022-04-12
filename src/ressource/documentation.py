4c17ae9104e1f984c278ac9ec3fb7f225ce15d0d
Vincent-vst <vincent.descatoire@gmail.com>
2022-04-12 13:06:58 +0000

    update doc

--- src/ressource/documentation.py
+++ src/ressource/documentation.py
@@ -3,4 +3,4 @@ from flask_restful import Resource
 
 class Documentation(Resource) : 
     def get(self) : 
-        return "API Documentation : In order to use this API, do ... " 
+        return "<h3>API Documentation</h3>" +  "<a href='https://github.com/Vincent-vst/Davinci/tree/venv'>Read the doc</a> " 
