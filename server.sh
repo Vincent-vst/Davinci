aa477056cdae592916b65af7382a1f2ca1272c1c
“Vincent <louise.insomnia@gmail.com>
2022-04-19 09:22:49 +0200

    chores (server.sh) : formating tabs

--- server.sh
+++ server.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-
-
 echo -n "[port] : "
 
-read port 
+read port
 
 echo -n "[localhost] :"
 
@@ -13,23 +11,23 @@ hostname -i | awk '{print $2}'
 echo '---------------'
 
 {
-
-		python3 main.py  
-
-		
-	} || {
-
-			echo "error"1>&2 
-
-		while true; 
-
-					do echo -e 'http://10.16.0.250 OK\n\n$(iostat)' \ 
-
-									nc -l -k -p $port -q 1; 
-
-						done 
-
-						
-	}
+    
+    python3 main.py
+    
+    
+    } || {
+    
+    echo "error"1>&2
+    
+    while true;
+    
+    do echo -e 'http://10.16.0.250 OK\n\n$(iostat)' \
+        
+        nc -l -k -p $port -q 1;
+        
+    done
+    
+    
+}
 
 
