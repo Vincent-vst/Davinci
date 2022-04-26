94b3927b0f8daf6c00f0dae3fbfa86126b298c7a
Vincent-vst <vincent.descatoire@gmail.com>
2022-04-26 14:46:10 +0000

    feats (*.db) : modifying db

--- tests/db.py
+++ tests/db.py
@@ -1,15 +1,23 @@
 import sqlite3
 
-conn = sqlite3.connect("davinci.db")
+conn = sqlite3.connect("tapwebapi.db")
 
 cursor = conn.cursor()
-sql_query = """ create table workers (
-    id integer primary key, 
+sql_query = """ create table tapjoblist (
+    id integer primary key autoincrement,
+    user text,
     task text not null, 
     pwd text not null, 
-    status text not null, 
-    audio_sample text not null
+    audio_sample json, 
+    priority integer, 
+    eta time,
+    status text not null
 )
 
 """
+
+# example de requete : 
+# insert into tapjoblist (user, task, pwd, audio_sample, priority, eta, status) values ('vincent', 'IL', '/tmp', '{"name": "vincent"}', 2, datetime(), 'failure'); 
+
+
 cursor.execute(sql_query)
